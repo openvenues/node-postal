248e94c603c3ee176141c5a3f49027440a5ee336
Al <albarrentine@gmail.com>
2016-02-11 11:33:43 -0500

    [fix] Removing console.log

--- test/index.test.js
+++ test/index.test.js
@@ -17,7 +17,6 @@ describe('expand', function() {
         });
         it('should handle non-ASCII addresses correctly from Node', function() {
             expansions = new sets.Set(postal.expand.expand_address('Friedrichstraße 128, Berlin, Germany'));
-            console.log(expansions);
             assert(expansions.has('friedrich strasse 128 berlin germany'));
         });
 
